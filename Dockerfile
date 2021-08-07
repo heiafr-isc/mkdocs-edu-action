f4b6c9df65cf80dad6d9a9f657ee1befa3cf2adc
Jacques Supcik <jacques.supcik@hefr.ch>
2021-08-07 15:41:20 +0200

    wip

--- Dockerfile
+++ Dockerfile
@@ -6,4 +6,5 @@ RUN  pip install --upgrade pip
 COPY requirements.txt /tmp/
 RUN pip install -r /tmp/requirements.txt
 
-ENTRYPOINT ["mkdocs"]
+COPY entrypoint.sh /entrypoint.sh
+ENTRYPOINT ["/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,16 @@
+set -o errexit
+set -o pipefail
+set -o nounset
+# set -o xtrace
+
+# Set magic variables for current file & dir
+__dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+__file="${__dir}/$(basename "${BASH_SOURCE[0]}")"
+__base="$(basename ${__file} .sh)"
+
+site_dir="${1:-public}"
+
+python --version
+pip --version
+mkdocs --version
+mkdocs build -c -d ${site_dir}
